783b29ca188f26a6da7c99788a42e63936bb3c16
abduznik <85239936+abduznik@users.noreply.github.com>
2025-12-02 19:41:15 +0200

    Change page title from 'MyGarden' to 'Useless Wiki'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "MyGarden",
+    pageTitle: "Useless Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
